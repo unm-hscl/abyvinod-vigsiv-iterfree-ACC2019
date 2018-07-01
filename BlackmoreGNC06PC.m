f3331829d5da4b6fb124843a9d2c2f6c28d94bd4
vigsiv <vigsiv@unm.edu>
2018-06-30 20:37:53 -0600

    Updated the state vector generation pre-input.

--- BlackmoreGNC06PC.m
+++ BlackmoreGNC06PC.m
@@ -25,7 +25,7 @@ clear, clc, close all
                       0 0.5*delT^2;
                       0       delT;];
     % Disturbance matrix of the 2D double integrator: 
-        G = [1; 1; 1; 1;];
+        G = [1 1 1 1];
 
 % Number of particles to generate. 
     N = 100; 
@@ -34,33 +34,31 @@ clear, clc, close all
     T = 30;
     
 % Randomly generate the disturbance vector from the standard normal.
-    W = mvnrnd(zeros(1,length(A)*N),eye(length(A)*N))';
-    plot(W(:),'+')
+    for k = 1:N
+        W(:,:,k) = mvnrnd(zeros(1,length(A)*T),eye(length(A)*T))';
+    end
+%     plot(W(:),'+')
 
 % Consider the initial state is known:
     x0 = [1,0,2,0]';
 
 % Generate future state trajectories now we have sampled the disturbance.
     
-    % Preallocate concatnated x vector: 
-        X = zeros(1,length(W))';
-        X(1:length(x0)) = x0;
-    % Preallocate A vector: 
-        Ac = zeros(length(A)*length(X),length(A));
-        Ac(1:length(A),1:length(A)) = eye(length(A)); 
-        Ac(length(A)+1:2*length(A),1:length(A)) = A; 
-        
+    % Preallocate concatnated xP vector (pre-input applied): 
+        Xp = zeros(length(W),1,N);
+        Xp(1:4,:,:) = repmat(x0,1,1,N);
     
     % Generate state tracjectories: 
-        for i = 1:length(X)
-            
-            if i == 1
-                Ac(1:length(A),1:length(A)) = eye(length(A));
-                X(i+4:i+8) = Ac*X(i:i+4)+G
-            elseif i == 2
-                Ac(length(A)+1:2*length(A),1:length(A)) = A;
+        for i = 1:N
+            for j = 1:T
+                if j == 1
+                    Xp((4*(j)+1):4*(j+1),:,i) = A*Xp(1:4,:,i)+G*W(1:4,:,i);
+                elseif j == T
+                    break
+                else
+                    Xp((4*(j)+1):4*(j+1),:,i) = A*Xp(4*(j-1):4*j-1,:,i)+G*W(4*(j-1):4*j-1,:,i);
+                end
             end
-            
         end
         
 
