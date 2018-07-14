2d1933d3381a47f02947f5e08f9b411b1e997c00
vigsiv <vigsiv@unm.edu>
2018-07-13 21:14:14 -0600

    updated with some old code.

--- BlackmoreTRo11PC_Ono08.m
+++ BlackmoreTRo11PC_Ono08.m
@@ -30,11 +30,53 @@ Delta = 0.1;
         B = [0; 0.033]; 
     % Define the matrices of the discrete time system: 
        Ad = [];
-       Bd = zeros(size(A,2)*(T+1),size(u,1)); 
-       Gd = zeros(size(A,2)*T,size(w,1));
+       Bd = zeros(size(A,2)*(T+1),size(size(B,1)*T,1)); 
+       Gd = zeros(size(A,2)*T,size(A,2)*(T+1));
         
     % Preallocate the trajectory vectors to be used: 
        x = zeros(size(A,2)*(T+1),1); 
-       u = ones(size(B,1)*T,1);
+       u = zeros(T,1);
        w = zeros(size(x,1),N);
+   
+     % Define the matrices of the discrete time system: 
+       Ad = [];
+       Bd = zeros(size(A,2)*(T+1),size(u,1)); 
+       Gd = zeros(size(A,2)*T,size(w,1));
        
+       for i = 1:(T+1)
+           
+           Ad = [Ad; A^(i-1);];
+           
+       end 
+       
+       for i = 1:(T+1)
+           if i == 1
+               
+                Ab{i}= zeros(size(A,2),size(B,2)); 
+               
+           else
+               
+                Ab{i}=A^(i-2)*B; 
+                
+           end
+    
+       end
+       
+       No = numel(Ab);
+       X = tril(true(No));
+       Y = hankel(ones(No,1));
+       [R,C] = find(Y);
+       U = accumarray(R,find(X),[],@(n){n});
+       Z = cell(No);
+       for k = 1:No
+           Z(U{k}) = Ab(k);
+       end
+       Z(~X) = {zeros(size(Ab{1}))};
+       Bp = cell2mat(Z);
+       Bd(:,1:size(u,1)) = Bp(:,1:size(u,1));
+
+       % Randomly generate the disturbance vector from the standard normal.
+        for i = 1:N
+            w(:,i) = mvnrnd(zeros(1,length(A)*(T+1)),eye(length(A)*(T+1)))';
+        end
+        w(1:2,1:N) = repmat([0;0],1,N); 
