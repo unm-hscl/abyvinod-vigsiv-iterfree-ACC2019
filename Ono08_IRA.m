37b9471f8b79a9b35b4e5f11f9c63d1af585318f
vigsiv <vigsiv@unm.edu>
2018-07-27 15:25:06 -0600

    Updated with some notation

--- Ono08_IRA.m
+++ Ono08_IRA.m
@@ -1,16 +1,16 @@
 %% Ono_IRA 2008 code
 % Coder: Vignesh Sivaramakrishnan
 % 
-clc, clear, close all
+clc, clear%, close all
 
 % Time Horizon: 
 
-T = 10; 
+T = 30; 
 
 
 % Maximum/minimum bound on input: 
 
-ulim = 0.2; 
+ulim = 1; 
 
 % System matrices: 
     % Sampling time of the discrete system:
@@ -65,12 +65,12 @@ ulim = 0.2;
        Bd = Bd(:,1:end-1);
        
 % Randomly generate the disturbance vector from the standard normal.
-    cov_mat_diag = 0.001*diag([1 0;]); 
+    cov_mat_diag = 0.0001*diag([1 0;]); 
     cov_mat = kron(eye(T+1),cov_mat_diag); 
     
 % Initial conditions: 
-    x0 = [0.1;0];
-
+    x0 = [0.4;0];
+    xtarget = linspace(-0.495,0,T)';
 % Generate nominal x (Note this is a code snippet taken from SReachTools):
 %     mean_concat_disturb = kron(ones(time_horizon,1), ...
 %                                sys.dist.parameters.mean);
@@ -89,9 +89,10 @@ ulim = 0.2;
 % Generate bounds: 
     h = [-1 0; 1 0;];
     hbig = kron(eye(T),h);
-    g = 0.16*[1;1];
-    gbig = repmat(g,T,1);
-    
+%     g = 0.5*[1;1];
+%     gbig = repmat(g,T,1);
+    g = linspace(0.5,0.1, T);
+    gbig = kron(g,[1,1])';
 %% Ono's converge alpha value
 alpha_on_iter = @(n) 0.7 * (0.98)^n;
 
@@ -107,14 +108,12 @@ desired_accuracy = 0.001;
 %% Compute M --- the number of polytopic halfspaces to worry about
 no_linear_constraints = size(hbig,1);
 
-Delta = 0.05;
+Delta = 0.005;
 
 %% Prepration for iterated risk allocation
 % Variables (re)initialized
 iter_count = 0;                 % No. of iterations done in Ono's risk
                                 % allocation algorithm
-opt_value_prev = 10000;         % Previous optimal value --- exit cond.
-opt_value = 0;                  % Optimal value --- |slack variables|_1
 % Given Delta, construct delta_i as Delta/M
 delta_vec = Delta/no_linear_constraints * ones(no_linear_constraints,1);
 
@@ -122,11 +121,9 @@ delta_vec = Delta/no_linear_constraints * ones(no_linear_constraints,1);
 %% Compute \sqrt{h_i^\top * \Sigma_X_no_input * h_i}
 sigma_vector = sqrt(diag(hbig*cov_X_sans_input(3:end,3:end)*hbig'));
 N_active = no_linear_constraints;
-Delta_residual = Delta;
 % Converge to one more decimal precision
 while N_active > 0 && iter_count < 50
     %% Store the previous optimal value
-    opt_value_prev = opt_value;
 
     %% Solve the feasibility problem
     % Construct the back-off (Hessem's term) in the constraints
@@ -142,8 +139,6 @@ while N_active > 0 && iter_count < 50
             abs(U_vector) <= ulim; 
             hbig*mean_X(3:end)<=gbig - scaled_norminv + slack_variables;
     cvx_end
-
-    opt_value = norm(slack_variables,1);
 
     %% Number of active/infeasible constraints via complementary
     %% slackness --- non-zero slack variables imply infeasible \delta_i
@@ -182,13 +177,14 @@ while N_active > 0 && iter_count < 50
     % \delta_i \gets \delta_i + \delta_residual/N_active 
     delta_vec(active_indx) = delta_vec(active_indx) +...
         delta_residual / N_active;
-    [iter_count N_active opt_value delta_vec(2:2:end)']
+    [iter_count N_active cvx_optval delta_vec(2:2:end)']
     %% Update the iteration count
     iter_count = iter_count + 1;
 end
-min_delta_true_with_motion = 1-mvncdf(-repmat(g(1),1,T),repmat(g(2),1,T),mean_X(3:2:end)', cov_X_sans_input(3:2:end,3:2:end));    
+% min_delta_true_with_motion = 1-mvncdf(-repmat(g(1),1,T),...
+%     repmat(g(2),1,T),mean_X(3:2:end)', cov_X_sans_input(3:2:end,3:2:end));    
 
-fprintf('Done with Delta: %1.4f, N_active: %2d ',...
+fprintf('Done with Delta: %1.4f, N_active: %2d\n',...
         Delta,...
         N_active);
 if N_active == 0 
@@ -197,96 +193,123 @@ if N_active == 0
     disp(' Found a feasible initial delta');
 
 else
-    % All constraints are active, then no solution could be found ---
-    % Dream lower (increase delta)
-    Delta_lb = Delta;
-    disp(' Given Delta requirement is not feasible.');
+    error('Given Delta requirement is not feasible.');
 end
+% figure()
+% plot(1:(T+1),mean_X(1:2:end))    
+% hold on
+% plot(1:(T+1), -g(1)*ones(1,T+1),'r')
+% plot(1:(T+1), g(2)*ones(1,T+1),'r')
+% plot(1:(T+1), xtarget*ones(1,T+1),'g')
+% xlabel('time')
+% ylabel('x')
+% title('Init delta');
+disp('Now working on control objective');
 
-%      %% Prepration for iterated risk allocation
-%         % Variables (re)initialized
-%         iter_count = 0;                 % No. of iterations done in Ono's risk
-%                                         % allocation algorithm
-%         opt_value_prev = 10000;         % Previous optimal value --- exit cond.
-%         opt_value = 0;                  % Optimal value --- |slack variables|_1
-%         % Converge to one more decimal precision
-%         while abs(opt_value - opt_value_prev) >= desired_accuracy/10
-%             %% Store the previous optimal value
-%             opt_value_prev = opt_value;
-% 
-%             %% Solve the feasibility problem
-%             % Construct the back-off (Hessem's term) in the constraints
-%             scaled_norminv=sigma_vector.*...
-%                               norminv(ones(no_linear_constraints,1)- delta_vec);
-%             cvx_begin 
-%                 variable U_vector(size(B,2)*T,1);
-%                 variable mean_X(size(mean_X_sans_input,1), 1);
-%                 minimize (sum(abs(U_vector))+(0.02-sum(abs(mean_X))));
-%                 subject to
-%                     mean_X == mean_X_sans_input + Bd*U_vector; 
-%                     abs(U_vector) <= ulim; 
-% %                    abs(mean_X)<=0.06;
-%                     hbig*mean_X<=gbig-scaled_norminv; 
-%             cvx_end
-%             
-%             opt_value = sum(abs(U_vector));
-% 
-%             %% Number of active/infeasible constraints via complementary
-%             %% slackness --- non-zero slack variables imply infeasible \delta_i
-%             N_active=nnz(slack_variables >= myeps);
-% 
-%             %% Break if N_active is zero or all are active
-%             if N_active == 0 || N_active == no_linear_constraints
-%                 break;
-%             end
-% 
-%             %% Compute \alpha value --- decides on how quickly inactive \delta_i
-%             %% are tightened: 
-%             alpha = alpha_on_iter(iter_count); 
-% 
-%             %% For all the inactive constraints, \delta_i^+ \gets \alpha
-%             %% \delta_i + (1-\alpha) (1-normcdf(g-hx/sigma_vec(i)))
-%             inactive_indx = find(slack_variables < myeps);
-%             % Compute relevant g-hx^\ast
-%             correction_deltas = gbig(inactive_indx) - ...
-%                 hbig(inactive_indx,:) * mean_X;
-%             % Update inactive delta
-%             delta_vec(inactive_indx) = alpha * delta_vec(inactive_indx) +...
-%                 (1 - alpha) * (1 - normcdf(...
-%                     correction_deltas./sigma_vector(inactive_indx)));
-% 
-%             %% Collect the headroom gained: \delta_residual \gets \Delta -
-%             %% \sum_i \delta_i
-%             delta_residual = Delta - sum(delta_vec);
-% 
-%             %% For all the active constraints, distribute the remaining headroom
-%             active_indx = find(slack_variables >= myeps);
-%             % \delta_i \gets \delta_i + \delta_residual/N_active 
-%             delta_vec(active_indx) = delta_vec(active_indx) +...
-%                 delta_residual / N_active;
-% 
-%             %% Update the iteration count
-%             iter_count = iter_count + 1;
-%         end
-%         fprintf('Done with Delta: %1.4f, N_active: %2d ',...
-%                 Delta,...
-%                 N_active);
-%         if N_active == 0 
-%             % If no constraint is active, then a feasible input policy has been
-%             % found --- Dream higher (decrease delta)
-%             Delta_ub = Delta;
-%             lb_stoch_reach_avoid = 1-Delta;
-%             optimal_input_vector = U_vector;
-%             disp(' Aim for more probability');
-%         
-%         else
-%             % All constraints are active, then no solution could be found ---
-%             % Dream lower (increase delta)
-%             Delta_lb = Delta;
-%             disp(' Aim for less probability');
-%         end
+%% Prepration for iterated risk allocation
+% Variables (re)initialized
+iter_count = 1;                 % No. of iterations done in Ono's risk
+                                % allocation algorithm
+opt_value_prev = 10000;         % Previous optimal value --- exit cond.
+opt_value = 0;                  % Optimal value --- |slack variables|_1
+% Given delta_vec that is feasible we will now solve for a control
+% objective. 
 
+
+%% Compute \sqrt{h_i^\top * \Sigma_X_no_input * h_i}
+sigma_vector = sqrt(diag(hbig*cov_X_sans_input(3:end,3:end)*hbig'));
+input_state_ratio = 0.0001;
+opt_value_array(1) = (input_state_ratio*sum(abs(U_vector))/(ulim*T) + sum(abs(mean_X(3:2:end)-xtarget))/(2*g(1)*T));
+% Converge to one more decimal precision
+while abs(opt_value_prev - opt_value) >  1e-4
+    %% Store the previous optimal value
+    opt_value_prev = opt_value;
+
+    %% Solve the feasibility problem
+    % Construct the back-off (Hessem's term) in the constraints
+    scaled_norminv=sigma_vector.*...
+                      norminv(ones(no_linear_constraints,1)- delta_vec);
+    cvx_precision BEST
+    cvx_begin quiet
+        variable U_vector(size(B,2)*T,1);
+        variable mean_X(size(mean_X_sans_input,1), 1);
+        minimize (input_state_ratio*sum(abs(U_vector))/(ulim*T) + sum(abs(mean_X(3:2:end)-xtarget))/(2*g(1)*T));
+        subject to
+            mean_X == mean_X_sans_input + Bd*U_vector; 
+            abs(U_vector) <= ulim; 
+            hbig*mean_X(3:end)<=gbig - scaled_norminv;
+    cvx_end
+    if ~strcmp(cvx_status, 'Solved')
+        keyboard
+    end
+    opt_value = cvx_optval;
+    opt_value_array(iter_count+1) = opt_value;
     
+    %% Number of active/infeasible constraints via complementary
+    %% slackness --- non-zero slack variables imply infeasible \delta_i
+    N_active=nnz( abs(hbig*mean_X(3:end) - gbig + scaled_norminv)<myeps);
+
+    %% Break if N_active is zero or all are active
+    if N_active == 0
+        disp('No issues with the current delta allocation');
+        break;
+    elseif N_active == no_linear_constraints
+        error('Not possible to solve');
+    end
+
+    %% Compute \alpha value --- decides on how quickly inactive \delta_i
+    %% are tightened: 
+    alpha = alpha_on_iter(iter_count); 
+
+    %% For all the inactive constraints, \delta_i^+ \gets \alpha
+    %% \delta_i + (1-\alpha) (1-normcdf(g-hx/sigma_vec(i)))
+    inactive_indx = find(hbig*mean_X(3:end) < gbig - scaled_norminv);
+    % Compute relevant g-hx^\ast
+    correction_deltas = gbig(inactive_indx) - ...
+        hbig(inactive_indx,:) * mean_X(3:end);
+    % Update inactive delta
+    delta_vec(inactive_indx) = alpha * delta_vec(inactive_indx) +...
+        (1 - alpha) * (1 - normcdf(...
+            correction_deltas./sigma_vector(inactive_indx)));
+    %% Collect the headroom gained: \delta_residual \gets \Delta -
+    %% \sum_i \delta_i
+    if Delta - sum(delta_vec) < 0
+        disp('Breaking early since we got a negative residual');
+        break;
+    end
+    delta_residual =  Delta - sum(delta_vec);
+
+    %% For all the active constraints, distribute the remaining headroom
+    active_indx = find(abs(hbig*mean_X(3:end) - gbig + scaled_norminv)<myeps);
+    % \delta_i \gets \delta_i + \delta_residual/N_active 
+    delta_vec(active_indx) = delta_vec(active_indx) +...
+        delta_residual / N_active;
+%     [iter_count N_active opt_value]
+    %% Update the iteration count
+    iter_count = iter_count + 1;
+end
+% min_delta_true_with_motion = 1-mvncdf(-repmat(g(1),1,T),repmat(g(2),1,T),mean_X(3:2:end)', cov_X_sans_input(3:2:end,3:2:end));    
+
+fprintf('Done with Delta: %1.4f, N_active: %2d\n',...
+        Delta,...
+        N_active);
+%%
+figure(1)
+hold on
+plot(1:(T+1), [-gbig(1);-gbig(1:2:end)],'r')
+plot(1:(T+1), [gbig(2);gbig(2:2:end)],'r')
+plot(2:(T+1), xtarget,'go')
+plot(1:(T+1),mean_X(1:2:end))    
+xlabel('time')
+ylabel('x')
+title('Trajectory')
+
+figure(2);
+hold on
+plot(opt_value_array)
+xlabel('iteration count')
+ylabel('cost')
+title('Final J cost');
 % % Generate intial delta and do some housekeeping: 
 %     del = Delta/((T+1)*size(h,1)); 
 %     delta = repmat(del,size(h,1)*(T+1),1);
