3773d2aa319fbd35026adf86f63bf511135c9451
vigsiv <vigsiv@unm.edu>
2018-08-04 16:39:38 -0600

    slight mods.

--- RolleLerp.m
+++ RolleLerp.m
@@ -5,7 +5,7 @@ function [cdf_approx_m, cdf_approx_c,varargout] = RolleLerp()
 % Define the second derivative of error function as a standin for norminv.
 % This is done by evaluating the symbolic expression as follows: 
 
-
+syms x; d = diff(erfinv(2*x-1),x,1)
 
 
 
