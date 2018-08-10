3b9d55bf7d525467ccace819ea4dfe75b301ef2d
Abraham <aby.vinod@gmail.com>
2018-08-10 14:28:10 -0600

    Add PwlOver+PwlUnder approximation + test

--- getPWLOverAndUnderApprox.m
+++ getPWLOverAndUnderApprox.m
@@ -1,58 +1,96 @@
-function [cdf_overapprox_m, cdf_overapprox_c, cdf_underapprox_m, cdf_underapprox_c] = getPWLOverAndUnderApprox(lbdelta,ubdelta,maxlierror)
+function [cdf_overapprox_m,...
+          cdf_overapprox_c,...
+          cdf_underapprox_m,...
+          cdf_underapprox_c] = getPWLOverAndUnderApprox(lbdelta,ubdelta,eta,g_matlabfun,funtype)
+    warning('getPWLOverAndUnderApprox does not check for monotonicity and concavity claims.');
 
-    myeps = 1e-10;
     %% Initialization
     knots_underapprox(1) = lbdelta;
-    knots_overapprox(1) = lbdelta; % To be overwritten
-    hval(1) = 0;                   % To be overwritten
     j = 1;
-    search_interval = [myeps 0.1];
+    search_interval_min = 0;
+    search_interval_max = ubdelta - lbdelta;
     fzero_options = optimset('Display','off'); % show iterations
                 
     %% Symbolic function definitions
-    syms x;
-    syms h;
     % TODO: Get g as an argument
     % Function definition for Phi^{-1}(1-x)
-    g = -sqrt(2)* erfinv(2*(1 - x) -1 );
     % First derivative
+    syms x;
+    syms h;
+    syms hmax;
+    g = g_matlabfun(x);
     g1diff=diff(g,1);
     % Second derivative: Function in (x,h) that needs to be solved for h given x,
-    % d^2/dh^2 g(x+h) == 8 * eta^-;
+    % d^2/dh^2 g(x+h) == 2 * eta^-;
     g2diff=diff(g,2);
-    g_maxerror=subs(g2diff,x+h)*h^2 + 8*maxlierror;
+    % Due to concavity of g, the error becomes more negative for larger h
     
     % Iterate till we reach the end
     while knots_underapprox(j)< ubdelta
-        % Set up the function to be solved by fzero
-        g_maxerror_at_j = subs(g_maxerror,x,knots_underapprox(j));
-        g_maxerror_at_j_mf = matlabFunction(g_maxerror_at_j);
-        % Set up the search interval to not exceed ubdelta
-        search_interval(2) = min(search_interval(2), ubdelta - knots_underapprox(j));
-        % Solve for h
-%         fprintf('%1.4e\n',search_interval);
-%         fprintf('%1.4e\n',g_maxerror_at_j_mf(search_interval));
-        [hval(j)] = fzero(g_maxerror_at_j_mf,search_interval,fzero_options);
-        % Ensure that the knot chosen does not exceed
-        knots_underapprox(j+1) = min(knots_underapprox(j) + hval(j),ubdelta);
+
+        if strcmpi(funtype,'mono-inc')
+            hval = sqrt(-8*eta/double(subs(g2diff,x,knots_underapprox(j))));
+            knots_underapprox(j+1) = knots_underapprox(j) + hval;
+        elseif strcmpi(funtype,'mono-dec')
+            g_maxerror=subs(g2diff,x+h)*h^2 + 8*eta;
+            g_maxerror_at_j = subs(g_maxerror,x,knots_underapprox(j));
+            g_maxerror_at_j_mf = matlabFunction(g_maxerror_at_j);
+            % Set up the search interval to not exceed ubdelta
+            search_interval = [search_interval_min  min(search_interval_max,ubdelta-knots_underapprox(j))];
+            % Solve for h
+            try
+                hval = fzero(g_maxerror_at_j_mf,search_interval,fzero_options);
+                knots_underapprox(j+1) = min(knots_underapprox(j) + hval,ubdelta);
+            catch
+                % If errored, then the search interval doesn't see a sign flip
+                % => we have reached the end
+                hval = ubdelta - knots_underapprox(j);
+                knots_underapprox(j+1) = ubdelta;
+            end
+%             fprintf('%2d. Searched in [%1.4e,%1.4e] to get %1.4e\n',j, knots_underapprox(j)+search_interval,knots_underapprox(j+1));
+        else
+            disp('Untested');
+%             %% Use these steps in a general case --- Untested
+%             g_maxerror=subs(g2diff,x+hmax)*h^2 + 8*eta;
+%             g_maxerror_at_j = subs(g_maxerror,x,knots_underapprox(j));
+%             g_maxerror_at_j_mf = matlabFunction(g_maxerror_at_j);
+%             % Set up the search interval to not exceed ubdelta
+%             search_interval = [search_interval_min  search_interval_max];
+%             % Solve for h
+%             try
+%                 max_soln = fmincon(@(v) g_maxerror_at_j_mf(v(1),v(2)),[1,1],[],[],[],[],search_interval_min*ones(2,1),search_interval_max*ones(2,1));
+%                 hval(j) = max_soln(2);
+%                 knots_underapprox(j+1) = min(knots_underapprox(j) + hval(j),ubdelta);
+%             catch
+%                 % If errored, then the search interval doesn't see a sign flip
+%                 % => we have reached the end
+%                 hval(j) = ubdelta - knots_underapprox(j);
+%                 knots_underapprox(j+1) = ubdelta;
+%             end
+%             fprintf('%2d. Searched in [%1.4e,%1.4e] to get %1.4e\n',j, knots_underapprox(j)+search_interval,knots_underapprox(j+1));
+        end
         
         %% Construction of the underapproximation of a concave function --- Lagrange interpolation
-        y_2 = norminv(1-knots_underapprox(j+1));
-        y_1 = norminv(1-knots_underapprox(j));
+        y_2 = double(subs(g,knots_underapprox(j+1)));
+        y_1 = double(subs(g,knots_underapprox(j)));
         x_2 = knots_underapprox(j+1);
         x_1 = knots_underapprox(j);
         % Lagrange linear interpolation
         cdf_underapprox_m(j) = (y_2 - y_1)/(x_2 - x_1);
         cdf_underapprox_c(j) = y_1 - cdf_underapprox_m(j) * x_1;
         
-%         %% Construction of the overapproximation of a concave function --- first-order Taylor series
-%         % Set up the gradient function
-%         g1diff_at_j = subs(g1diff - cdf_underapprox_m(j),x,knots_underapprox(j));
-%         g1diff_at_j_mf = matlabFunction(g1diff_at_j);
-%         % Solve for Taylor series operating point
-%         knots_overapprox(j) = fzero(g1diff_at_j_mf,[x_1 x_2],fzero_options);        
-%         cdf_overapprox_m(j) = double(subs(g1diff,knots_overapprox(j)));
-%         cdf_overapprox_c(j) = double(subs(g,knots_overapprox(j))) - double(subs(g1diff,knots_overapprox(j))) * knots_overapprox(j);
+        %% Construction of the overapproximation of a concave function --- first-order Taylor series
+        % Set up the gradient function
+        g1diff_at_j = g1diff - cdf_underapprox_m(j);
+        g1diff_at_j_mf = matlabFunction(g1diff_at_j);
+        % Set up the search interval as [x(j), x(j+1)]
+        search_interval = [x_1  x_2];
+        % Search for hvalgrad such that f'(x(j)+hvalgrad) = c_{j} ---
+        % existence guaranteed by mean value theorem
+        [x_grad_match] = fzero(g1diff_at_j_mf,search_interval,fzero_options);
+        %cdf_underapprox_m(j) is the same as double(subs(g1diff,x_grad_match));
+        cdf_overapprox_m(j) = cdf_underapprox_m(j);
+        cdf_overapprox_c(j) = double(subs(g,x_grad_match)) - cdf_overapprox_m(j) * x_grad_match;        
         
         %% Increment j
         j = j+1;       
