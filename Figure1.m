e9097e15908ebca46ef6c9ebfb9d29df04ace275
Vignesh Sivaramakrishnan <vigneshsivaramakrishnan@Vigneshs-MacBook-Pro.local>
2019-02-11 21:03:46 -0700

    Completed Figure1.m

--- Figure1.m
+++ Figure1.m
@@ -2,16 +2,16 @@ clc, clear, close all
 
 
 %% norminv(1-delta)
-maxlierror=1e-5;
+maxlierror=1e-2;
 g = @(z) -z.^2;
 fun_monotone = 'mono-dec';
-lower_bound = 1E-3;
-upper_bound = 2; 
-no_of_test_points = 1000;
-function_handle = @(z) -g(z);
+lower_bound = maxlierror;
+upper_bound = 4; 
+no_of_test_points = 100;
+function_handle = @(z) g(z);
 
 x_iter=linspace(lower_bound,upper_bound,no_of_test_points);
-y_iter_true = -function_handle(x_iter);     % Only th
+y_iter_true = function_handle(x_iter);     % Only th
 
 [PWA_overapprox_m, PWA_overapprox_c,...
     PWA_underapprox_m, PWA_underapprox_c] = getPWAOverAndUnderApprox(...
@@ -21,11 +21,43 @@ y_iter_true = -function_handle(x_iter);     % Only th
 % Also, because of the image across x-axis due to negation, the definitions
 % of approximations get flipped
 if length(PWA_overapprox_m) > 1
-    y_iter_underapprox  = max(-PWA_overapprox_m' *x_iter - PWA_overapprox_c');
-    y_iter_overapprox = max(-PWA_underapprox_m'*x_iter - PWA_underapprox_c');
+    y_iter_underapprox  = -max(-PWA_overapprox_m' *x_iter - PWA_overapprox_c');
+    y_iter_overapprox = -max(-PWA_underapprox_m'*x_iter - PWA_underapprox_c');
 else
-    y_iter_underapprox  = -(PWA_overapprox_m' *x_iter + PWA_overapprox_c');
-    y_iter_overapprox = -(PWA_underapprox_m'*x_iter + PWA_underapprox_c');
+    y_iter_underapprox  = PWA_overapprox_m' *x_iter + PWA_overapprox_c';
+    y_iter_overapprox = PWA_underapprox_m'*x_iter + PWA_underapprox_c';
 end    
 err_overapprox=y_iter_overapprox-y_iter_true;
 err_underapprox=y_iter_true-y_iter_underapprox;
+
+%% Plotting
+
+fig1 = figure(1);
+hold on
+polyvertex1 =[x_iter,x_iter(1);y_iter_overapprox,y_iter_overapprox(end)]'; % Note: This needs MPT to run!!
+P1 = Polyhedron('V',polyvertex1);
+polyvertex2 =[x_iter,x_iter(1);y_iter_true,y_iter_true(1)]'; % Note: This needs MPT to run!!
+P2 = Polyhedron('V',polyvertex2);
+polyvertex3 =[x_iter,x_iter(1);y_iter_underapprox,y_iter_underapprox(end)]'; % Note: This needs MPT to run!!
+P3 = Polyhedron('V',polyvertex3);
+h1 = plot(P3,'color','y');
+hold on
+h2 = plot(P2,'color','b');
+h3 = plot(P1,'color','r');
+axis([0.009 0.275 -0.11 0.025])
+% yticks([-1 -0.5 0])
+set(fig1,'Units','centimeters');
+set(fig1,'Position',[0 0 12 4]);
+grid on
+box on
+set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
+set(groot, 'defaultLegendInterpreter','latex');
+set(groot, 'defaulttextInterpreter','latex');
+legend({'$l_f^+\geq s$','$f(x)\geq s$','$l_f^-\geq s$'},'Location','eastoutside','FontSize',12)
+xlabel('x')
+ylabel('s')
+set(gca,'FontSize',12)
+fig1 = tightfig(fig1);
+
+
+
